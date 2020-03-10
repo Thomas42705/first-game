5f01164869eeed2bd8edbf92201d011f1c78dec0
Thomas42705 <61979110+Thomas42705@users.noreply.github.com>
2020-03-10 10:29:50 -0400

    first commit

---
+++ Main.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+export (PackedScene) var Mob
+var score
+
+func _ready():
+	randomize()
+	
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+
+
+func new_game():
+	score = 0
+	$player.start($StartPosition.position)
+	$StarTimer.start()
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnlocation.set_offset(randi())
+	var mob = Mob.instance()
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	mob.position = $MobPath/MobSpawnLocation.position 
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction 
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	

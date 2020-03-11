1e7c5edc14204c247ec99d8dddb1b39cb08d27b9
Thomas42705 <61979110+Thomas42705@users.noreply.github.com>
2020-03-11 10:34:47 -0400

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -36,4 +36,6 @@ func _on_MobTimer_timeout():
 	direction += rand_range(-PI / 4, PI / 4)
 	mob.rotation = direction 
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
 	
+
